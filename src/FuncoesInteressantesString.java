241ae486a1f7f9fc3512576fc6692c3059852470
xYviil <viniciusmeurer2012@gmail.com>
2024-09-10 16:43:18 -0300

    terceiro commit

--- src/FuncoesInteressantesString.java
+++ src/FuncoesInteressantesString.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class FuncoesInteressantesString {
+public class FuncoesInteressantesString {
+
+    public static void main(String[] args) {
+
+        String original = "abcde FGHIJ ABC abc DEFG       ";
+
+        String s01 = original.toLowerCase();
+        String s02 = original.toUpperCase();
+        String s03 = original.trim();
+        String s04 = original.substring(2);
+        String s05 = original.substring(2, 9);
+        String s06 = original.replace("a", "x");
+        String s07 = original.replace("abc", "xyz");
+        int i = original.indexOf("bc");
+        int j = original.lastIndexOf("bc");
+
+
+        System.out.println("Original : " + original + "-");
+        System.out.println("toLowerCase : " + s01 + "-");
+        System.out.println("toUpperCase - " + s02 + "-");
+        System.out.println("trim : " + s03 + "-");
+        System.out.println("Substring(2) : " + s04 + "-");
+        System.out.println("Substring(2,9):" + s05 + "-");
+        System.out.println("replace('a', 'x'):" + s06 + "-");
+        System.out.println("replace('abc', 'xyz'):" + s07 + "-");
+        System.out.println("indexOf 'bc':" + i);
+        System.out.println("lastIndexOf 'bc':" + j);
+
+    String s = "potato apple lemon orange";
+
+    String[] vect = s.split(" ");
+
+    System.out.println(vect[0]);
+    System.out.println(vect[1]);
+    System.out.println(vect[2]);
+    System.out.println(vect[3]);
+    }
+
 }
