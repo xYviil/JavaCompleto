2fbbee1a0f5e4fccc8ca30e37a7169d590df9b5f
xYviil <viniciusmeurer2012@gmail.com>
2024-09-14 23:15:02 -0300

    nono commit

--- src/POO/Problema1.java
+++ src/POO/Problema1.java
@@ -24,11 +24,9 @@ public class Problema1 {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p- x.c));
 
-         p = (y.a +y.b + y.c) / 2;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p- y.c));
+        double areaX = x.area();
+        double areaY = y.area();
 
         System.out.printf("Área do triangulo X: %.4f%n", areaX);
         System.out.printf("Área do triangulo Y: %.4f%n", areaY);
