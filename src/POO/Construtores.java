a0cc328eb8eab27ffffb30752e21c1a7b9093111
xYviil <viniciusmeurer2012@gmail.com>
2024-09-29 23:29:32 -0300

    décimo oitavo commit

--- src/POO/Construtores.java
+++ src/POO/Construtores.java
@@ -9,15 +9,13 @@ public class Construtores {
 
         Scanner sc = new Scanner(System.in);
 
-
+        Product p = new Product();
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
         String name = sc.nextLine();
         System.out.print("Price: ");
         double price = sc.nextDouble();
-        System.out.print("Quantity: ");
-        int quantity = sc.nextInt();
-        Product product = new Product(name, price, quantity);
+        Product product = new Product(name, price);
         System.out.println();
         System.out.println("Product data: " + product);
         System.out.println();
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -6,12 +6,23 @@ public class Product {
     public double price;
     public int quantity;
 
+
+    public Product(){
+
+    }
+
+
     public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
-
     }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
 
     public double totalValueInStock() {
         return price * quantity;
