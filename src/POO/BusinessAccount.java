54b032025c4019945f543c1e6ad7314d99949e3c
xYviil <viniciusmeurer2012@gmail.com>
2024-12-08 18:10:09 -0300

    quadragésimo terceiro commit

--- src/POO/BusinessAccount.java
+++ src/POO/BusinessAccount.java
@@ -25,7 +25,14 @@ public class BusinessAccount extends Account {
 
     public void loan(double amount) {
         if (amount <= loanLimit) {
-            balance += amount - 10 ;
+            balance += amount - 10;
         }
     }
+
+    @Override
+    public  void withdraw(double amount) {
+        super.withdraw(amount);
+        balance -= 2.0;
+    }
+
 }
