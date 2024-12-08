2706759dda381356440c22e746210eb5c5aa6829
xYviil <viniciusmeurer2012@gmail.com>
2024-12-08 17:44:06 -0300

    quadragésimo segundo commit

--- src/POO/Heranca.java
+++ src/POO/Heranca.java
@@ -1,11 +1,37 @@
 package POO;
 
+import entities.Account;
+import entities.SavingsAccount;
+
 public class Heranca {
 
     public static void main(String[] args) {
 
-        BusinessAccount account = new BusinessAccount();
+        Account acc = new Account(1001, "Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
+        // Upcasting
 
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Vinícius", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+        // Downcasting
+
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+        acc4.loan(100);
+
+        //BusinessAccount acc5 = (BusinessAccount) acc3;
+        if(acc3 instanceof BusinessAccount) {
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if (acc3 instanceof SavingsAccount) {
+            SavingsAccount acc5 = (SavingsAccount)acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
     }
 }
