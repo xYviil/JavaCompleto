ce25b708d6b114ea80bae62c8eead4e3e3b802dd
xYviil <viniciusmeurer2012@gmail.com>
2024-12-24 23:55:33 -0300

    quadragésimo oitavo commit

---
+++ src/POO/try_catch.java
@@ -0,0 +1,47 @@
+package POO;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class try_catch {
+
+public static void main(String[] args) {
+
+    method1();
+
+    System.out.println("Fim do programa!");
+
+}
+
+public static void method1() {
+    System.out.println("***METHOD1 START***");
+    method2();
+    System.out.println("***METHOD1 END***");
+}
+
+public static void method2() {
+    System.out.println("***METHOD2 START***");
+
+    Scanner sc = new Scanner(System.in);
+
+    try {
+
+        String[] vect = sc.nextLine().split(" ");
+    int position = sc.nextInt();
+    System.out.println(vect[position]);
+}
+catch (ArrayIndexOutOfBoundsException e ) {
+    System.out.println("Posição inválida!");
+    e.printStackTrace();
+    sc.next();
+}
+catch (InputMismatchException e) {
+    System.out.println("Input error!");
+}
+sc.close();
+
+System.out.println("***METHOD2 END***");
+
+}
+
+}
---
+++ src/POO/try_catch_finally.java
@@ -0,0 +1,31 @@
+package POO;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class try_catch_finally {
+
+    public static void main(String[] args) {
+
+        File file = new File("C:\\Users\\vinic\\OneDrive\\Imagens\\Screenshots\\in.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        }
+        finally {
+            if (sc != null) {
+                sc.close();
+            }
+            System.out.println("Finally block executed!");
+        }
+    }
+
+}
